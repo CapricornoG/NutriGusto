90da6997a877f659c6c4fefd96945c13b66ec6bd
Giuseppe <dimontegiuseppe.1@gmail.com>
2025-03-18 12:50:24 +0100

    feat: fix template issue and load home page correctly

--- nutrigusto/settings.py
+++ nutrigusto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'nutrigusto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'home' / 'templates'],  # This should point to the correct folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
